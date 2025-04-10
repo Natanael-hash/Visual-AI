0cef98e95ae2a2d57641c542de77d21f1c74e5ff
Natanael-hash <natanaelhordon@yahoo.com>
2025-04-10 11:47:06 +0300

    Add directions for avoid obstacles

--- README.md
+++ README.md
@@ -47,16 +47,16 @@ This will open the web interface in your default browser, typically at `http://l
  - **Launch the app and point your smartphone camera at the desired surroundings.**
    ```bash
       # The initial script serves as a prototype, where I utilized the model to analyze its accuracy 
-      # by applying it to a 4K video.
+      # by applying it to a 4K video or image.
       from ultralytics import YOLO
-      model = YOLO("best.pt")
+      model = YOLO("object_detection.pt")
       model.predict("path/to/video/or/image", save=True, imgsz=640, conf=0.5, device="mps", show=True)
       # This script is designed for Apple Silicon MacBooks that support Metal 
       # for the integrated graphics card. It enables the use of the iPhone's camera 
       # for real-time detection. To utilize the MacBook's webcam, replace the first parameter (0) with 1, and the model will 
       # perform real-time detection from the laptop's camera.
       from ultralytics import YOLO
-      model = YOLO("best.pt")
+      model = YOLO("object_detection.pt")
       model.predict(0, save=True, imgsz=640, conf=0.5, device="mps", show=True)
    ```
 - **The app will identify and describe objects or obstacles in real time.**
--- src/data_prediction/yolo_predictions_and_distance_estimation.py
+++ src/data_prediction/yolo_predictions_and_distance_estimation.py
@@ -3,6 +3,7 @@ import cv2
 from time import time
 from ultralytics import YOLO
 import depth_pro
+import pyttsx3
 
 class ObjectDetection_and_Distance_Estimation:
     def __init__(self, capture_index):
@@ -13,6 +14,8 @@ class ObjectDetection_and_Distance_Estimation:
         self.depth_model, self.transform = depth_pro.create_model_and_transforms()
         self.depth_model = self.depth_model.to(self.device)
         self.depth_model.eval()
+        self.engine = pyttsx3.init()
+        self.engine.setProperty('rate', 100)
 
     @staticmethod
     def load_yolo_model():
@@ -78,29 +81,75 @@ class ObjectDetection_and_Distance_Estimation:
             cv2.rectangle(frame, (rect_x1, rect_y1), (rect_x2, rect_y2), (0, 0, 0), -1)
             cv2.putText(frame, text, (text_x, text_y), font, font_scale, (255, 255, 255), font_thickness)
 
-        return frame
+        return frame, object_boxes, depth
+
+    def get_directions(self, frame, object_boxes, depth):
+        image_height, image_width = frame.shape[:2]
+        third_width = image_width // 3
+        left_side = third_width
+        front_side = 2 * third_width
+        right_side = image_width
+        
+        # cv2.rectangle(frame, (0, 0), (left_side, image_height), (0, 255, 0), 2)
+        # cv2.rectangle(frame, (left_side, 0), (front_side, image_height), (0, 255, 255), 2)
+        # cv2.rectangle(frame, (front_side, 0), (right_side, image_height), (255, 0, 0), 2)
+
+        left_obstacles = []
+        right_obstacles = []
+        front_obstacles = []
+
+        for x1, y1, x2, y2, class_name in object_boxes:
+            center_x = (x1 + x2) // 2
+            center_y = (y1 + y2) // 2
+            depth_value = depth[center_y, center_x]
+
+            
+            if center_y > image_height * 0.75:
+                continue
+
+            if center_x < left_side:
+                left_obstacles.append((class_name, depth_value))
+            elif center_x < front_side:
+                front_obstacles.append((class_name, depth_value))
+            else:
+                right_obstacles.append((class_name, depth_value))
+
+
+        if any(d < 1.5 for _, d in front_obstacles):
+            left_blocked = any(d < 1.5 for _, d in left_obstacles)
+            right_blocked = any(d < 1.5 for _, d in right_obstacles)
+
+            if left_blocked and right_blocked:
+                self.engine.say(f"{class_name} ahead. No space left or right. Please stop.")
+            elif not left_blocked:
+                self.engine.say(f"{class_name} ahead. Go around it to the left.")
+            elif not right_blocked:
+                self.engine.say(f"{class_name}ahead. Go around it to the right.")
+
+        self.engine.runAndWait()      
 
     def __call__(self):
         cap = cv2.VideoCapture(self.capture_index)
         assert cap.isOpened(), "Error: Could not open video capture."
-
+    
         while True:
             ret, frame = cap.read()
             assert ret, "Error: Could not read frame."
-
+    
             start_time = time()
             results = self.predict(frame)
-            frame = self.plot_bboxes_and_depth_estimation(results, frame)
-            
+            frame, object_boxes, depth = self.plot_bboxes_and_depth_estimation(results, frame)
+            self.get_directions(frame, object_boxes, depth)
+    
             end_time = time()
-
+    
             fps = 1 / (end_time - start_time)
             cv2.putText(frame, f'FPS: {fps:.2f}', (10, 90), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
-
+    
             cv2.imshow('YOLO and Depth Estimation', frame)
-
+    
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-
+    
         cap.release()
         cv2.destroyAllWindows()
